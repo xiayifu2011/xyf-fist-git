b11ddba4a89cca152a80a0046a20ee4db8359985
xiayifu2011 <19218770812@163.com>
2026-02-11 19:34:59 +0800

    Add files via upload

--- 整理/模板/线性筛.cpp
+++ 整理/模板/线性筛.cpp
@@ -12,11 +12,13 @@ void find_prime(int upper)
     for (int i = 2; i <= upper; i++)//
     {
         if (!is_prime[i])
-        {
-            prime.push_back(i);
-        }
+            {
+                prime.push_back(i);
+            }
+        
         for (int j : prime)
         {
+            
             if (i * j > upper)
             {
                 break; // 防止越界
@@ -26,21 +28,26 @@ void find_prime(int upper)
             {
                 break;
             }
+            
             // i % j == 0
             // 换言之，i 之前被 j 筛过了
             // 由于 prime 里面质数是从小到大的，所以 i 乘上其他的质数的结果一定会被
             // j 的倍数筛掉，就不需要在这里先筛一次，所以这里直接 break
             // 掉就好了
         };
+        
     }
 }
 
 signed main()
 {
+    
     cin >> n;
+
     find_prime(n);
     for (int i : prime)
     { // 遍历prime数组
+
         cout << i << " ";
     }
     return 0;
